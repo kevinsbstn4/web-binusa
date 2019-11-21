021ad346f90f855adac19ed801512d05d77261a1
kevinsbstn4 <kevincaesar22@gmail.com>
2019-11-21 08:49:10 +0700

    first

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/7ee979efb5.js" crossorigin="anonymous"></script>
+<style>
+.btn {
+  background-color: white;
+  border: none;
+  color: black;
+  padding: 16px 32px;
+  text-align: center;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition: 0.3s;
+  font-size: 12px;
+}
+
+
+.btn:hover {
+  background-color: #2196F3;
+  color: white;
+}
+</style>
+    <title>Document</title>
+</head>
+<body >
+<ul class="nav" style="background-color: #ffbc66">
+  <li class="nav-item">
+    <a style="color: white; margin-left: 90px;" class="nav-link active" href="#">Mikrotik Academy</a>
+  </li>
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">Bursa Kerja Khusus</a>
+  </li> 
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">Kelulusan</a>
+  </li>
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">E-Book</a>
+  </li>
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">Pendaftaran</a>
+  </li>
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">Pendaftaran</a>
+  </li>
+</ul>
+<ul class="nav" style="background-color: #fc5400; height:100px;">
+  <img style="margin-left: 90px; height:78px; margin-top: 10px;" src="http://binusasmg.sch.id/wp-content/uploads/2016/11/logobinus.png" />
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">profile</a>
+  </li>
+  <li class="nav-item">
+    <a style="color: white;" class="nav-link" href="#">kurikulum</a>
+  </li>
+</ul>
+<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+    <img src="https://binusasmg.sch.id/wp-content/uploads/2016/11/binus-mikrotik-2.jpg" class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="https://binusasmg.sch.id/wp-content/uploads/2016/11/binus-mikrotik-2.jpg" class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<center>
+<div style="margin-top: 20px; width: 1100px; height: 900px;" class="card">
+  <div class="card-body">
+  <span style="background-color: #2196F3; padding: 8px; color: white; border-radius: 5px; font-size: 10px; margin-left: 200px;" class="label info"><b>TUE 11</b></span>
+  <a style="font-size: 13px;"><b>VIDEO PROFIL JURUSAN TATA BUSANA SMK BINA NUSANTARA SEMARANG</b></a> 
+  <br>
+  <i class="fas fa-tags"></i>
+ <button class="btn">Akademik</button>
+ <button class="btn">Berita</button><br>
+ <i class="fas fa-comments"></i>
+ 
+  </div>
+</div>
+</center>
+</body>
+</html>
